a627eb92573929e282d3c5e6423005ebc78490a0
MK <mk@ifrog.ru>
2017-01-20 15:24:44 +0300

    Added support for multiple sending of letters and modified letter template

--- siteAvailable.sh
+++ siteAvailable.sh
@@ -1,65 +1,92 @@
 #!/bin/bash
 
-TO_MAIL="maks.kutin@gmail.com"
-FROM_MAIL="no-reply@mkutin.ru"
+# Constants
+FROM_MAIL="no-reply@ifrog.ru"
 SERVER_NAME=$HOSTNAME
+SITE_LIST="site.txt"
+EMAIL_LIST="email.txt"
+SUBJECT_MAIL="Отчет о проверке доступности сайтов"
 
+# Lists for responce 
+bad_list=""
+ok_list=""
+
+# Get alias from site.txt
 index=0
-
-# Write alias of site into array from site.txt
 while read line; do 
 	aliasArray[$index]="$line"
 	index=$(($index+1))
-done < site.txt
+done < $SITE_LIST
+
+# Get email from email.txt
+index=0
+while read line; do 
+	emailArray[$index]="$line"
+	index=$(($index+1))
+done < $EMAIL_LIST
 
 # Get http response with status
 for ((a=0; a < ${#aliasArray[*]}; a++)); do
+    CHECK_TIME=$(date +"%d-%m-%Y %H:%M:%S")
+
     status=`curl --write-out %{http_code} --silent --output /dev/null ${aliasArray[$a]}`
 
     # Check status code
     if [ "$status" != "200" ] && [ "$status" != "301" ]; then
-		SUBJECT_MAIL="Проблемы с доступом к сайту"
-		HTML="Оповещение о проблеме доступа к сайту"
-		CHECK_TIME=$(date +"%d-%m-%Y %H:%M:%S")
-
-		SUBJECT_MAIL="$SUBJECT_MAIL ${aliasArray[$a]}"
-		HTML="
-			<html>
-				<head>
-					<title>$SUBJECT_MAIL</title>
-					<style>
-						body {
-							font-family: monospace;
-						}
-
-						a {
-							text-decoration: none;
-							color: black;
-						}
-
-						.important {
-							color: red;
-						}
-
-						.info {
-							line-height: 20px;
-							text-align: center;
-						}
-					</style>
-				</head>
-
-				<body>
-					<h2 align='center'>Проблемы с доступностью сайта</h2>
-					<hr />
-					<p class='info'>$HTML <b>${aliasArray[$a]}</b>.<br />Статус возвращаемого ответа - <span class='important'>$status</span><br />Проверка в $CHECK_TIME</p>
-					<hr />
-					<p align='right'>Отправлено с $SERVER_NAME</p>
-				</body>
-			</html>
-		"
-
-		MAIL_TXT="Subject: $SUBJECT_MAIL\nFrom: $FROM_MAIL\nTo: $TO_MAIL\nContent-Type: text/html\nMIME-Version: 1.0\n\n$HTML"
-
-		echo -e $MAIL_TXT | /usr/sbin/sendmail -t
+		bad_list="$bad_list<li><p class='info'>Недоступен сайт <b>${aliasArray[$a]}</b>.<br />Статус возвращаемого ответа - <span class='bad_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>"
+	else
+		ok_list="$ok_list<li><p class='info'> С адресом <b>${aliasArray[$a]}</b> все в порядке.<br />Статус возвращаемого ответа - <span class='ok_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>"
 	fi
+done
+
+# Render mail 
+HTML="
+	<html>
+		<head>
+			<title>$SUBJECT_MAIL</title>
+			<style>
+				body {
+					font-family: monospace;
+				}
+
+				a {
+					text-decoration: none;
+					color: black;
+				}
+
+				.bad_code {
+					color: red;
+				}
+
+				.ok_code {
+					color: green;
+				}
+
+				.info {
+					line-height: 20px;
+					text-align: center;
+				}
+			</style>
+		</head>
+
+		<body>
+			<h2 align='center'>Автоматический отчет сервера о проверке доступности сайтов</h2><br />
+			<p align='left'>Следующие сайты <span class='bad_code'>не ответили положительным статусом</span>:</p>
+			<hr />
+				<ol>$bad_list</ol>
+			<hr /><br />
+			<p align='left'>Следующие сайты <span class='ok_code'>ответили положительным статусом</span>:</p>
+			<hr />
+				<ol>$ok_list</ol>
+			<hr />
+			<p align='right'>Отправлено с $SERVER_NAME</p>
+		</body>
+	</html>
+"
+
+# Send email to someone
+for ((a=0; a < ${#emailArray[*]}; a++)); do
+    MAIL_TXT="Subject: $SUBJECT_MAIL\nFrom: $FROM_MAIL\nTo: ${emailArray[$a]}\nContent-Type: text/html; charset=utf-8\nMIME-Version: 1.0\n\n$HTML"
+
+    echo -e $MAIL_TXT | /usr/sbin/sendmail -t
 done
