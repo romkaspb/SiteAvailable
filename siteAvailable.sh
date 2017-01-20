7fe7053b7848bdd1ce5389f5c13feb2fd4c12137
MK <mk@ifrog.ru>
2017-01-20 15:48:33 +0300

    Small changes

--- siteAvailable.sh
+++ siteAvailable.sh
@@ -6,10 +6,11 @@ SERVER_NAME=$HOSTNAME
 SITE_LIST="site.txt"
 EMAIL_LIST="email.txt"
 SUBJECT_MAIL="Отчет о проверке доступности сайтов"
+FLAG=false
 
 # Lists for responce 
 bad_list=""
-ok_list=""
+# ok_list=""
 
 # Get alias from site.txt
 index=0
@@ -34,8 +35,10 @@ for ((a=0; a < ${#aliasArray[*]}; a++)); do
     # Check status code
     if [ "$status" != "200" ] && [ "$status" != "301" ]; then
 		bad_list="$bad_list<li><p class='info'>Недоступен сайт <b>${aliasArray[$a]}</b>.<br />Статус возвращаемого ответа - <span class='bad_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>"
-	else
-		ok_list="$ok_list<li><p class='info'> С адресом <b>${aliasArray[$a]}</b> все в порядке.<br />Статус возвращаемого ответа - <span class='ok_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>"
+
+		FLAG=true
+	# else
+	# 	ok_list="$ok_list<li><p class='info'> С адресом <b>${aliasArray[$a]}</b> все в порядке.<br />Статус возвращаемого ответа - <span class='ok_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>"
 	fi
 done
 
@@ -74,19 +77,16 @@ HTML="
 			<p align='left'>Следующие сайты <span class='bad_code'>не ответили положительным статусом</span>:</p>
 			<hr />
 				<ol>$bad_list</ol>
-			<hr /><br />
-			<p align='left'>Следующие сайты <span class='ok_code'>ответили положительным статусом</span>:</p>
-			<hr />
-				<ol>$ok_list</ol>
 			<hr />
 			<p align='right'>Отправлено с $SERVER_NAME</p>
 		</body>
 	</html>
 "
+if [ FLAG ]; then
+	# Send email to someone
+	for ((a=0; a < ${#emailArray[*]}; a++)); do
+	    MAIL_TXT="Subject: $SUBJECT_MAIL\nFrom: $FROM_MAIL\nTo: ${emailArray[$a]}\nContent-Type: text/html; charset=utf-8\nMIME-Version: 1.0\n\n$HTML"
 
-# Send email to someone
-for ((a=0; a < ${#emailArray[*]}; a++)); do
-    MAIL_TXT="Subject: $SUBJECT_MAIL\nFrom: $FROM_MAIL\nTo: ${emailArray[$a]}\nContent-Type: text/html; charset=utf-8\nMIME-Version: 1.0\n\n$HTML"
-
-    echo -e $MAIL_TXT | /usr/sbin/sendmail -t
-done
+	    echo -e $MAIL_TXT | /usr/sbin/sendmail -t
+	done
+fi
