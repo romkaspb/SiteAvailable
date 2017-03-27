073aa43ba28dd7d39aa9a49191bd8c8386e0ce3e
MK <mk@ifrog.ru>
2017-03-27 16:48:31 +0300

    Code refactoring and adding loop parallelization

--- siteAvailable.sh
+++ siteAvailable.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Constants
-FROM_MAIL="no-reply@ifrog.ru"
+START=$(date +%s)
+FROM_MAIL="no-reply@example.ru"
 SERVER_NAME=$HOSTNAME
-SITE_LIST="site.txt"
-EMAIL_LIST="email.txt"
+SITE_LIST="sites.txt"
+EMAIL_LIST="emails.txt"
+OK_LIST="ok.txt"
+BAD_LIST="bad.txt"
 SUBJECT_MAIL="Отчет о проверке доступности сайтов"
-FLAG=false
-
-# Lists for responce 
-bad_list=""
-# ok_list=""
 
 # Get alias from site.txt
 index=0
@@ -30,17 +28,25 @@ done < $EMAIL_LIST
 for ((a=0; a < ${#aliasArray[*]}; a++)); do
     CHECK_TIME=$(date +"%d-%m-%Y %H:%M:%S")
 
-    status=`curl --write-out %{http_code} --silent --output /dev/null ${aliasArray[$a]}`
+    status=$(curl --write-out %{http_code} --silent --output /dev/null ${aliasArray[$a]}) && {
+    	echo "$a. ${aliasArray[$a]} - $status"
 
-    # Check status code
-    if [ "$status" != "200" ] && [ "$status" != "301" ]; then
-		bad_list="$bad_list<li><p class='info'>Недоступен сайт <b>${aliasArray[$a]}</b>.<br />Статус возвращаемого ответа - <span class='bad_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>"
+    	# Check status code
+	    if [ "$status" != "200" ] && [ "$status" != "301" ] && [ "$status" != "000" ]; then
+			echo "$bad_list<li><p class='info'>Недоступен сайт <b>${aliasArray[$a]}</b>.<br />Статус возвращаемого ответа - <span class='bad_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>" >> $BAD_LIST
 
-		FLAG=true
-	# else
-	# 	ok_list="$ok_list<li><p class='info'> С адресом <b>${aliasArray[$a]}</b> все в порядке.<br />Статус возвращаемого ответа - <span class='ok_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>"
-	fi
+			FLAG=$(($FLAG+1))
+		else
+			echo "$ok_list<li><p class='info'>Доступен сайт <b>${aliasArray[$a]}</b>.<br />Статус возвращаемого ответа - <span class='ok_code'>$status</span><br />Проверка в $CHECK_TIME</p></li>" >> $OK_LIST
+		fi
+    } &
 done
+
+wait
+
+# Lists for responce 
+bad_list=`cat $BAD_LIST`
+ok_list=`cat $OK_LIST`
 
 # Render mail 
 HTML="
@@ -78,15 +84,31 @@ HTML="
 			<hr />
 				<ol>$bad_list</ol>
 			<hr />
+			<hr />
+				<ol>$ok_list</ol>
+			<hr />
 			<p align='right'>Отправлено с $SERVER_NAME</p>
 		</body>
 	</html>
 "
-if [ FLAG ]; then
-	# Send email to someone
+if [ -n "$bad_list" ]; then
+	# Send email to someone from email.txt
 	for ((a=0; a < ${#emailArray[*]}; a++)); do
 	    MAIL_TXT="Subject: $SUBJECT_MAIL\nFrom: $FROM_MAIL\nTo: ${emailArray[$a]}\nContent-Type: text/html; charset=utf-8\nMIME-Version: 1.0\n\n$HTML"
 
 	    echo -e $MAIL_TXT | /usr/sbin/sendmail -t
 	done
 fi
+
+`rm $BAD_LIST` && `rm $OK_LIST`
+
+END=$(date +%s)
+
+DIFF=$(( $END - $START ))
+
+# echo "It took $DIFF seconds. START - $START, END - $END"
+
+# Send mail to programmer
+# MAIL_TXT="Subject: $SUBJECT_MAIL\nFrom: $FROM_MAIL\nTo: mk@ifrog.ru\nContent-Type: text/html; charset=utf-8\nMIME-Version: 1.0\n\n$HTML"
+
+# echo -e $MAIL_TXT | /usr/sbin/sendmail -t
